a2ae737f1947d3810115acc90638e5c5de341da2
unknown <adelekeyahaya05@gmail.com>
2023-02-18 12:21:10 -0800

    setting up files

---
+++ index.php
@@ -0,0 +1,26 @@
+api key = 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
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Writing 5 Marketting caption for your product</h1> 
+    </div>
+
+    <form action="">
+        <div>
+            <input type="text" name="prompt" placeholder="shoe, burger, etc">
+        </div>
+        <div>
+            <input type="submit" name="prompt" value="Generate">
+        </div>
+    </form>
+
+</body>
+</html>
