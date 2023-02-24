55144164b0165525aba68bd416c1da51f0e09d02
unknown <adelekeyahaya05@gmail.com>
2023-02-24 03:18:04 -0800

    Ui completed

---
+++ marketmagnet/generate.php
@@ -0,0 +1,270 @@
+<?php
+error_reporting(0);
+
+require __DIR__. '/vendor/autoload.php';
+
+use \Orhanerday\OpenAi\OpenAi;
+
+
+
+//require_once realpath(__DIR__. "/vendor/autoload.php");
+// use Dotenv\Dotenv;
+// $dotenv = Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
+
+//$api_key = getenv('OPENAI_API_KEY');
+
+if (isset($_POST['submit'])) {
+  
+$open_ai_key = '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';
+
+$open_ai = new OpenAi($open_ai_key);
+
+$prompt = $_POST['product'];
+$image = $_POST['image'];
+
+$complete = $open_ai->completion([
+    'model' => 'text-davinci-003',
+    'prompt' => 'Write 5 marketing social media caption for'.$prompt,
+    'temperature' => 0.9,
+    'max_tokens' => 350,
+    'frequency_penalty' => 0,
+    'presence_penalty' => 0.6,
+]);
+
+$response = json_decode($complete, true);
+$response = $response["choices"][0]["text"];
+
+$complete = $open_ai->image([
+    "prompt" => "$image",
+    "n" => 1,
+    "size" => "256x256",
+    "response_format" => "url",
+ ]);
+
+ $image = json_decode($complete, true);
+ $image = $image["data"]["0"]["url"];
+
+}
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>MarketMagnet</title>
+
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<style>
+
+.prompt-form label{
+  padding-bottom: 8px;
+}
+.prompt-form{
+  width: 100%;
+  border-top: 3px solid #47b2e4 !important;
+  border-bottom: 3px solid #47b2e4 !important;
+  padding: 30px !important;
+  background: #fff !important;
+  box-shadow: 0 0 24px 0 rgba(0, 0, 0, 0.12) !important;
+}
+
+ .form-group {
+  padding-bottom: 5px;
+}
+
+.prompt-form input:focus {
+  border-color: #47b2e4 !important;
+}
+
+
+
+</style>
+<body>
+    
+
+    
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+        <div class="container" data-aos="fade-up">
+  
+          <div class="section-title">
+            <h2>The perfect marketing caption, hashtags and images for your product</h2>
+            </div>
+  
+            <div class="col-lg-7 mx-auto mt-5 mt-lg-0 d-flex align-items-stretch ">
+
+              <form action="" method="POST" role="form" class="prompt-form">
+
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="name">Product Name</label>
+                    <input type="text" name="product" class="form-control" id="name" placeholder="e.g shoe, burger" >
+                  </div>
+
+                  <div class="form-group col-md-6">
+                    <label for="name">Product image type</label>
+                    <input type="text" class="form-control" name="image" id="email" placeholder="e.g shoe, burger" >
+                  </div>
+                </div>
+              
+               <div class="text-center mt-5"><button type="submit" name="submit" class="generate">Generate</button></div>
+
+               <script>
+                
+               </script>
+
+               <p class="generating">Generating...</p>
+
+               <script>
+                    let generate = document.querySelector('.generate');
+                        let generating = document.querySelector('.generating');
+
+
+                        generate.onclick = function() {
+                      generating.style.display = "block";
+                  };
+
+               </script>
+
+                <div class="copy-text">
+  
+                    <div class="text">
+                      <?php if (!isset($_POST['submit'])) {
+                       echo "";
+                        }else {
+                          echo $response;
+                        } ?>
+                    </div>
+
+                  <?php 
+
+                  if (isset($_POST['submit'])) {
+                  ?> 
+
+                  <button class="btn-copy-text" ><i class='bx bxs-copy-alt'></i></button>
+                  <?php
+                  }
+                  ?>
+                </div>
+
+
+                  <div class="product-image text-center" style="padding-top: 70px;">
+                    <img src="<?= $image ?>" alt="">
+                  </div>
+                  
+                  <p class="feedback">Your feedback will help us improve</p>
+                  <p><a href="index.php" >Give feedback</a></p>
+              </form>
+              
+            </div>
+            
+          </div>
+  
+        </div>
+        
+      </section><!-- End Contact Section -->
+
+
+<script>
+    let copyText = document.querySelector('.copy-text');
+
+    copyText.querySelector("button").addEventListener("click", function(e){
+      e.preventDefault();
+        let input = document.querySelector(".text");
+        let range = document.createRange();
+        range.selectNode(input);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        copyText.classList.add("active");
+        window.getSelection().removeAllRanges();
+        setTimeout(function(){
+            copyText.classList.remove("active");
+        }, 2500);
+
+    })
+
+</script>
+
+        <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+  
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
---
+++ marketmagnet/index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>MarketMagnet</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<style>
+  /* .btn-contact
+{
+  background: #47b2e4;
+  border: 0;
+  padding: 12px 34px;
+  color: #fff;
+  transition: 0.4s;
+  border-radius: 50px;
+} */
+</style>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.php">MarketMagnet</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <!-- <li><a class="nav-link scrollto" href="#about">About</a></li> -->
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Feedback</a></li>
+          <li><a class="getstarted scrollto" href="generate.php">Get Started</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>Better Solutions For Your Business</h1>
+          <h2>Introducing Market Magnet: Your All-in-One Social Media Marketing Solution</h2>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="generate.php" class="btn-get-started scrollto">Get Started</a>
+            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Services</h2>
+          <p>Our innovative marketing tool is designed to help you sell your products on social media with ease. With our advanced algorithm, we can generate high-quality captions that are tailored to your specific product and target audience. Say goodbye to the days of spending hours brainstorming captions - let Market Magnet do the heavy lifting for you.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-xl-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4><a href="">Caption</a></h4>
+              <p>we will provoide you a well-crafted caption to capture your audience's attention, pique their interest, and ultimately convince them to make a purchase.</p>
+            </div>
+          </div>
+
+          <div class="col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Hashtags</a></h4>
+              <p>Our tool makes it easy to create compelling images and find the perfect hashtags to reach your target audience.</p>
+            </div>
+          </div>
+
+          <div class="col-xl-4 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4><a href="">24/7 support</a></h4>
+              <p>We know that marketing is critical to the success of your business, which is why we offer 24/7 support.</p>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us section-bg">
+      <div class="container-fluid" data-aos="fade-up">
+
+        <div class="row">
+
+          <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+
+            <div class="content">
+              <h3>Whether you're a small business owner or a marketing professional, <strong> Market Magnet is the ultimate social media marketing solution.</strong></h3>
+              <p>
+                Try it out today and see the difference it can make for your brand!
+              </p>
+            </div>
+
+            <div class="accordion-list">
+              <ul>
+                <li>
+                  <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1"><span>01</span> Are you struggling to create captivating captions for your social media posts?  <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
+                    <p>
+                      we generate captions that has the power to turn a simple post into a persuasive marketing tool that drives sales and engagement.
+                    </p>
+                  </div>
+                </li>
+
+                <li>
+                  <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed"><span>02</span> Finding the right hashtags can be a time-consuming and frustrating. <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
+                    <p>
+                      With Market Magnet, you can easily discover relevant hashtags and incorporate them into your content for maximum impact.
+                    </p>
+                  </div>
+                </li>
+
+                <li>
+                  <a data-bs-toggle="collapse" data-bs-target="#accordion-list-3" class="collapsed"><span>03</span> Elevate your social media game and attract new customers. <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
+                    <p>
+                      Our tool empowers you to create stunning visuals and find the perfect hashtags to elevate your social media game and attract new customers.
+                    </p>
+                  </div>
+                </li>
+
+              </ul>
+            </div>
+
+          </div>
+
+          <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/why-us.png");' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="skills">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="100">
+            <img src="assets/img/skills.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
+            <h3></h3>
+            <p class="fst-italic">
+              Build your online presence, change your social media statistics by using our tool.
+            </p>
+            <p class="fst-italic">we are 100% relaible</p> 
+
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Skills Section -->
+
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="row">
+          <div class="col-lg-9 text-center text-lg-start">
+            <h3>Use marketMagnet today!</h3>
+            <p>With Market Magnet, you can elevate your social media game and attract more followers without breaking the bank. Our free tool empowers you to create captivating captions, discover the right hashtags, and ultimately boost your online presence..</p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="generate.php">Get started</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+    <!-- ======= Pricing Section ======= -->
+    <section id="pricing" class="pricing">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Pricing</h2>
+          <p>Don't let a lack of resources hold you back from achieving your social media goals. Market Magnet is the free tool that can help you boost your online presence and achieve.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 mx-auto" data-aos="fade-up" data-aos-delay="100">
+            <div class="box">
+              <h3>Free Plan</h3>
+              <h4><sup>$</sup>0<span>per month</span></h4>
+              <ul>
+                <li><i class="bx bx-check"></i> Generate unlimited response per day</li>
+                <li><i class="bx bx-check"></i> Hashtags</li>
+                <li><i class="bx bx-check"></i>Images</li>
+                <li><i class="bx bx-check"></i>Caption</li>
+              </ul>
+              <a href="generate.php" class="buy-btn">Get Started</a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Pricing Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Feedback</h2>
+        
+          </div>
+
+          <div class="col-lg-7 mt-5 mx-auto mt-lg-0 d-flex align-items-stretch">
+            <form action="feedback.php" method="post" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" required></textarea>
+              </div>
+              
+              <div class="text-center"><button type="submit" class="btn-contact">Send Message</button></div>
+       
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+  
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
